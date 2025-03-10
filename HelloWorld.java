6e56db464874cb1716c353e2df0abae1cb2a659f
PedroH <pedroh422silva@gmail.com>
2025-03-10 13:45:05 -0300

    message

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,50 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("☕️☕️☕️☕️☕️");
         System.out.println("Hello, World!");
+
+        // Write Your code here 💖
+
+        System.err.println("Connecting to server…");
+        System.err.println("Error: Connection timed out.");
+        System.err.println("Retrying…");
+
+        // System.err.println() is used to print error messages! 😵😵❌
+        // It is similar to System.out.println() but is used to print error messages.
+
+        int roomBox = 15;
+        int masterBedroom = 45;
+        int bathroom = 10;
+
+        System.out.println(roomBox);
+        System.out.println(masterBedroom);
+        System.out.println(bathroom);
+
+        // Integer values: byte, short, int, and long
+        // Floating point values: float and double
+        // Boolean values: boolean
+        // Characters: char
+
+        int age = 19;
+        int cookies = 0; // Did we finish a cookie pie? 🥧
+        double pi = 3.14;
+        double megapixels = 34.674423;
+        boolean favColorBlue = true;
+        boolean subscribed = false;
+        char theFirstLetter = 'a';
+        char hashtag = '#';
+        String wordleWord = "TAKER";
+        String artist = "Sabrina Carpenter";
+
+        double result = 0;
+
+        //Operação
+
+        result = 8 + 2; // result is now 10
+        result = 8 - 2; // result is now 6
+        result = 8 * 2; // result is now 16
+        result = 8 / 2; // result is now 4
+        result = 8 % 2; // result is now 0
+
+        System.out.println(result); // 0
     }
 }
