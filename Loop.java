7d0c54ad88bc1a643f50493aafbb786b257b5303
PedroH <pedroh422silva@gmail.com>
2025-03-11 16:38:37 -0300

    loop

---
+++ Loop.java
@@ -0,0 +1,11 @@
+public class Loop {
+    public static void main(String[] args) {
+        int num = 1;
+
+        while (num <= 100) { 
+            System.out.println(num);
+            num = num + 1; 
+ 
+        }
+    }
+}
