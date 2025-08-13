83fb1348d81cc786c1139aabad086ebb07e5e82c
PedroH <pedroh422silva@gmail.com>
2025-08-13 15:48:20 -0300

    commit

---
+++ Inicio.java
@@ -0,0 +1,7 @@
+public class Inicio {
+    public static void main(String[] args) {
+        System.out.println("Início do programa");
+        System.out.println("Criando o objeto aluno01");
+        System.out.print("agfafbiakgfafgbaj");
+    }
+}
