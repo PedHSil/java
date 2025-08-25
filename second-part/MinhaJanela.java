8cf40d4b7d986a9ad0fffdf8d756d0a8d332c7ce
PedroH <pedroh422silva@gmail.com>
2025-08-25 09:38:41 -0300

    label

--- second-part/MinhaJanela.java
+++ second-part/MinhaJanela.java
@@ -3,12 +3,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 class MinhaJanela extends JFrame {
     
     private JButton botaoA;
     private JButton botaoB;
     private JButton botaoC;
+    private JButton botaoD;
+    private JButton botaoE;
+
+    private JLabel label = new JLabel("Contador: 0");
 
     public MinhaJanela() {
         super("Minha Janela"); // título da janela
@@ -20,11 +25,16 @@ class MinhaJanela extends JFrame {
         botaoA = new JButton("Aperte A");
         botaoB = new JButton("Aperte B");
         botaoC = new JButton("Aperte C");
+        botaoD = new JButton("Aperte D");
+        botaoE = new JButton("Aperte E");
 
         // Adicionar os botões na janela
         add(botaoA);
         add(botaoB);
         add(botaoC);
+        add(botaoD);
+        add(botaoE);
+        add(label);
 
         // Ações dos botões
         botaoA.addActionListener(new ActionListener() {
@@ -40,8 +50,21 @@ class MinhaJanela extends JFrame {
                 dispose(); // fecha a janela
             }
         });
-
+        // Usando Lambda nos listeners
         botaoC.addActionListener(e -> System.out.println("Botão C foi apertado"));
+
+        botaoD.addActionListener(e -> System.out.println("Botão C foi apertado"));
+        
+        botaoE.addActionListener(new ActionListener() {
+            int i = 0;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = ("o botão foi apertado " + i++ + " vezes");
+                System.out.println(s);
+                label.setText(s);
+            }
+        });
 
         // Configurações básicas
         pack(); // ajusta ao tamanho dos componentes
